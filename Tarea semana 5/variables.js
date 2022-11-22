2c5f3a0cda301711112f4024ca6229f948ad2903
benjapro <xbenjaps4x@gmail.com>
2022-11-22 19:51:58 -0300

    tarea entregada

---
+++ Tarea semana 5/variables.js
@@ -0,0 +1,10 @@
+let provincia = "Buenos Aires"
+let localidad = "San Nicolas"
+let antiguedad = "2000"
+let provincia_y_localidad = provincia + " " + localidad
+
+provincia = "Cordoba"
+localidad = "Carlos paz"
+
+console.log(provincia, localidad, antiguedad);
+alert(provincia_y_localidad);
